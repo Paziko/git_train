5df8a4461796befb83aa1224ade1c571e1ba7e63
Andrey Piarenko <AndreyPisarenko66@yandex.ru>
2025-04-05 23:24:55 +0300

    Created incrementor(++) and decrementer(--)

---
+++ incrementer.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "stoi.cpp"
+using namespace std;
+
+
+int main(int args, char* argv[]) {
+	if (args <= 1) {
+        cout << "Error: Not enough arguments" << endl;
+        return 1;
+    }
+
+    int result = 0;
+    int i = 1;
+
+    while (i < args && !(result = stoi(argv[i]))) {
+        ++i;
+    }
+
+    cout << "Result = " << ++result << endl;
+
+    return 0;
+}
